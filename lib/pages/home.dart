f065f5545ab124cd9c5dea7433e2c82af592db07
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-09-07 14:05:11 +0800

    feat: Added PostCard Widget

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:my_message_reels_frontend/services/auth_service.dart';
-import 'package:my_message_reels_frontend/shared/types/dto/auth_login.dto.dart';
 import 'package:my_message_reels_frontend/widgets/app_page/app_page.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
+import 'package:my_message_reels_frontend/widgets/post_card/post_card.dart';
 
-// '/'
 class Home extends HookWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  final List<PostCard> mockCards = [
+    const PostCard(),
+    const PostCard(),
+    const PostCard(),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return const AppPage(appBarTitle: 'Room List', child: Placeholder());
+    return AppPage(
+      child: CardSwiper(
+          cardBuilder: (_, index, __, ___) {
+            return mockCards[index];
+          },
+          cardsCount: mockCards.length),
+    );
   }
 }
---
+++ lib/widgets/post_card/post_card.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class PostCard extends StatelessWidget {
+  const PostCard({super.key});
+
+  final 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
