92fa9b9d0f729faf921a0960612024405336b458
LoejeeMan <72646329+GuyWithBag@users.noreply.github.com>
2025-09-08 18:52:27 +0800

    feat: Added likes to postdto

--- lib/shared/types/dto/post.dto.dart
+++ lib/shared/types/dto/post.dto.dart
@@ -1,13 +1,16 @@
 class PostDto {
   final String content;
   final String authorId;
-  final List<String> replies;
+  final List<String>? replies;
+  final List<String> likes;
   final DateTime createdAt;
   final bool published = false;
 
-  PostDto(
-      {required this.content,
-      required this.authorId,
-      required this.replies,
-      required this.createdAt});
+  PostDto({
+    required this.content,
+    required this.authorId,
+    this.replies,
+    this.likes = const [],
+    required this.createdAt,
+  });
 }
